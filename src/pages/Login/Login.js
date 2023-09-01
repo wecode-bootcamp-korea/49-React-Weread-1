d13fac1787260c907bdda6fb96bc5a595e5504f9
kgy8987 <rlarldud8987@naver.com>
2023-09-01 17:21:32 +0900

    PR생성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,25 @@
 import React from "react";
+import "./Login.css";
 
-function Login() {
-    return <h1>여긴 Login</h1>;
-    
+const Login = () => {
+  return (
+    <div className="login">
+        <div className="splash">
+        <img className="img_1" src="./Logo.png" alt="이미지없음"></img>
+        <img className="img_2" src="./logo_wecode.png" alt="이미지없음"></img>
+        </div>
+        <div className="container">
+            <input className="input_wrap1" type="text" placeholder="이메일"/>
+            <input className="input_wrap2" type="password" placeholder="비밀번호"/>
+            <button className="login_btn" type="button">로그인</button>
+        </div>
+        <div className="login_option">
+          <p className="">회원 가입</p>
+          <span class="bar">|</span>
+          <p className="">비밀번호 찾기</p>
+        </div>
+    </div>
+  );
 };
 
 export default Login;
