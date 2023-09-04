4d566a9deba53041f6796746c46f6b99341bbd07
coolllleooo <buseness200824@gmail.com>
2023-09-04 19:35:53 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.scss";
 
 const Main = () => {
   const a ="/Logo.png"
   const b ="/logo_wecode.png"
+  const [input1, setInput] = useState();
+  const saveUserId =(event)=>{
+    setInput(event.target.value);
+  }
+
+  //console.log(input1);
   return (
     <div className="main">
   <div className="logo3">
@@ -16,11 +22,11 @@ const Main = () => {
             <h2>Login</h2>
             <div>
                 <label htmlFor='input_id'>ID : </label>
-                <input type='text' name='input_id' />
+                <input type='text' name='input_id' onChange={saveUserId}/>
             </div>
             <div>
                 <label htmlFor='input_pw'>PW : </label>
-                <input type='password' name='input_pw' />
+                <input type='password' name='input_pw' onChange={saveUserId} />
             </div>
             <div>
                 <button type='button'>Login</button>
