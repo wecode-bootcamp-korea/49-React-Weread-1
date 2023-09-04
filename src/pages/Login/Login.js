02677e2f0ac24107b25fa032a717ff13eb8ac4de
안병우 <byungwoo1802@gmail.com>
2023-09-04 12:35:03 +0900

    pr생성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,22 @@
 import React from "react";
+import "./Login.css";
 
-function Login() {
-    return <h1>여긴 Login</h1>;
-    
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="container">
+        <img src="/image/Logo.png" alt="큰 로고" className="one" />
+        <img src="/image/logo_wecode.png" alt="wecode" className="two" />
+        <div className="input">
+          <input type="text" placeholder="이메일" />
+          <input type="text" placeholder="비밀번호" />
+          <button className="loginBtn">로그인</button>
+        </div>
+      </div>
+      <div className="contensts">
+        <button className="joinBtn">회원가입</button>|
+        <button className="findPasswordBtn">비밀번호 찾기</button>
+      </div>
+    </div>
+  );
 };
-
-export default Login;
