40e44c5d4bd7f4640e9571eb9c15de83ae8133b1
kgy8987 <rlarldud8987@naver.com>
2023-09-04 14:50:32 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,22 @@
-import React from "react";
-import "./Login.css";
+import React, { useState } from "react";
+import "./Login.scss";
+
 
 const Login = () => {
+ 
+  const [id, setId] = useState(""); // eslint-disable-line no-unused-vars
+  const [pw, setPw] = useState(""); // eslint-disable-line no-unused-vars
+  
+  const saveUserld = event => {
+    setId(event.target.value); 
+  }; 
+
+  const saveUserPw = event => {
+    setPw(event.target.value); 
+  };
+
+  
+
   return (
     <div className="login">
         <div className="splash">
@@ -9,8 +24,8 @@ const Login = () => {
         <img className="img_2" src="./logo_wecode.png" alt="이미지없음"></img>
         </div>
         <div className="container">
-            <input className="input_wrap1" type="text" placeholder="이메일"/>
-            <input className="input_wrap2" type="password" placeholder="비밀번호"/>
+            <input className="input_wrap1" type="text" placeholder="이메일" onChange={saveUserld} required/>
+            <input className="input_wrap2" type="password" placeholder="비밀번호" onChange={saveUserPw} required/>
             <button className="login_btn" type="button">로그인</button>
         </div>
         <div className="login_option">
